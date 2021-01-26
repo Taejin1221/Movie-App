4e8f32f3a614449b7825a20d8e08ba3c84555e90
Taejin-Mac <taejin7824@gmail.com>
2021-01-26 19:44:31 +0900

    #2.0 Lecture
    Make my own component
    1. React can render only one component, in other word all components must be in App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import Potato from './Potato'
 
 function App() {
   return (
-    <div className="App"> Hi!!! </div>
+    <div className="App">
+      <h1> Hi!! </h1>
+      <Potato />
+    </div>
   );
 }
 
