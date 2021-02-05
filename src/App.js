7f4f7555c45bd0996d4f686ab847f02e52c4dce2
Jin-MacBook <taejin7824@gmail.com>
2021-02-05 17:19:39 +0900

    #2.3 map Recap Lecture Code
    Fix some errors
    1. img tag must has alt prop
    2. Every component should be different, so we use key prop and this is used by internal React
    Remember Thing
    1. React Component should be unique
    2. Use key prop to give component to uniqueness
    3. key prop is only used by internal React

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from "react";
 
 const alice_in_wonderland = [
   {
+    id: 1,
     name: "Alice",
     image: "https://upload.wikimedia.org/wikipedia/commons/6/63/Alice_par_John_Tenniel_04.png"
   }, {
+    id: 2,
     name: "White Rabbit",
     image: "https://upload.wikimedia.org/wikipedia/commons/e/ec/Down_the_Rabbit_Hole.png"
   }, {
+    id: 3,
     name: "Mad Hatter",
     image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/MadlHatterByTenniel.svg/1920px-MadlHatterByTenniel.svg.png"
   }, {
+    id: 4,
     name: "Queen of Hearts",
     image: "https://upload.wikimedia.org/wikipedia/commons/2/25/Queen_of_Hearts.jpg"
   }
@@ -20,7 +24,7 @@ function Information( {name, image} ) {
   return (
     <div className={name}>
       <h1> Name: {name} </h1>
-      <img src={image} alt="" />
+      <img src={image} alt={name} witdh="300" height="300" />
     </div>
   );
 }
@@ -29,8 +33,8 @@ function App() {
   return (
     <div className="App">
       <h1> Character of Alice in Wonderland </h1>
-      {alice_in_wonderland.map( infor => (
-        <Information name={infor.name} image={infor.image} />
+      {alice_in_wonderland.map( info => (
+        <Information key={info.id} name={info.name} image={info.image} />
        ) )}
     </div>
   );
