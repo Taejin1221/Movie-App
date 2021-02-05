7fe03467034b3c96db7d44db7fc0974839f41dc7
Jin-MacBook <taejin7824@gmail.com>
2021-02-05 11:54:17 +0900

    #2.2 React Lecture Codes
    Study how to send data dynamically
    -> Use js method like map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import React from "react";
 
-function Food( {fav} ) {
-  return <h1> I like {fav} </h1>;
+const alice_in_wonderland = [
+  {
+    name: "Alice",
+    image: "https://upload.wikimedia.org/wikipedia/commons/6/63/Alice_par_John_Tenniel_04.png"
+  }, {
+    name: "White Rabbit",
+    image: "https://upload.wikimedia.org/wikipedia/commons/e/ec/Down_the_Rabbit_Hole.png"
+  }, {
+    name: "Mad Hatter",
+    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/MadlHatterByTenniel.svg/1920px-MadlHatterByTenniel.svg.png"
+  }, {
+    name: "Queen of Hearts",
+    image: "https://upload.wikimedia.org/wikipedia/commons/2/25/Queen_of_Hearts.jpg"
+  }
+];
+
+function Information( {name, image} ) {
+  return (
+    <div className={name}>
+      <h1> Name: {name} </h1>
+      <img src={image} alt="" />
+    </div>
+  );
 }
 
 function App() {
   return (
     <div className="App">
-      <h1> Hi!! </h1>
-      <Food fav="grape"/>
-      <Food fav="potato"/>
-      <Food fav="maratang"/>
+      <h1> Character of Alice in Wonderland </h1>
+      {alice_in_wonderland.map( infor => (
+        <Information name={infor.name} image={infor.image} />
+       ) )}
     </div>
   );
 }
